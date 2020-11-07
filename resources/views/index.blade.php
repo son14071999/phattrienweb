a1aceddb6779e5788ee894ab066178e22adbac05
nhusonnb <son.trannhu.ncc@gmail.com>
2020-11-07 08:34:52 +0700

    add banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,12 @@
 <html lang="en">
 
 <head>
-  <title>Academics &mdash; Website by Colorlib</title>
-  <meta charset="utf-8">
+    <title>Academics &mdash; Website by Colorlib</title>
+    <meta charset="utf-8">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,900" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -17,13 +17,16 @@
     <link rel="stylesheet" type="text/css" href="{{asset('css/cssHeader.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/cssContent.css')}}">
     <link rel="stylesheet" href="{{asset('css/cssFooter.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('css/cssBody.css')}}" type="text/css">
+
 
 </head>
 <div class="container-fluid bg-secondary">
     @include('block.header')
-    <div id="content">
-        @yield('content')
-    </div>
+    {{--    <div id="content">--}}
+    {{--        @yield('content')--}}
+    {{--    </div>--}}
+    @include('block.body')
     @include('block.footer')
 
 </div>
@@ -46,7 +49,7 @@
 
 
 
-  <!-- loader -->
+<!-- loader -->
 
 
 </body>
