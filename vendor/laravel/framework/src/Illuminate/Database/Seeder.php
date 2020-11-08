2ae39552154e18dcb2c53dc560221ff9f68674cc
son14071999 <son1999tmgl@gmail.com>
2020-11-08 21:30:10 +0700

    test

---
+++ vendor/laravel/framework/src/Illuminate/Database/Seeder.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Illuminate\Database;
+
+use Illuminate\Console\Command;
+use Illuminate\Container\Container;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+
+abstract class Seeder
+{
+    protected $ma_truong = array(
+        'ussh'=>'khoa học xã hội và nhân văn',
+        'hus'=>'khoa học tự nhiên'
+    );
+    protected $donvi = array('đào tạo','công tác sinh viên');
+    protected $tieuchi = array(
+        'quy mô đào tạo đại học'=>array('số lượng sinh viên đào tạo đại học', 1),
+        'quy mô giảng viên'=>array('tỉ lệ giảng viên trên tổng qy mô đào tạo', 1),
+        'việc làm của người học'=>array('tỉ lệ đạo tạo sau đại học có việc làm ', 2)
+    );
+//    protected  $ten_truong = array(['Khoa học tự nhiên, khoa học xã hội và nhân văn']);
+    /**
+     * The container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
+
+    /**
+     * The console command instance.
+     *
+     * @var \Illuminate\Console\Command
+     */
+    protected $command;
+
+    /**
+     * Seed the given connection from the given path.
+     *
+     * @param  array|string  $class
+     * @param  bool  $silent
+     * @return $this
+     */
+    public function call($class, $silent = false)
+    {
+        $classes = Arr::wrap($class);
+
+        foreach ($classes as $class) {
+            $seeder = $this->resolve($class);
+
+            $name = get_class($seeder);
+
+            if ($silent === false && isset($this->command)) {
+                $this->command->getOutput()->writeln("<comment>Seeding:</comment> {$name}");
+            }
+
+            $startTime = microtime(true);
+
+            $seeder->__invoke();
+
+            $runTime = round(microtime(true) - $startTime, 2);
+
+            if ($silent === false && isset($this->command)) {
+                $this->command->getOutput()->writeln("<info>Seeded:</info>  {$name} ({$runTime} seconds)");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Silently seed the given connection from the given path.
+     *
+     * @param  array|string  $class
+     * @return void
+     */
+    public function callSilent($class)
+    {
+        $this->call($class, true);
+    }
+
+    /**
+     * Resolve an instance of the given seeder class.
+     *
+     * @param  string  $class
+     * @return \Illuminate\Database\Seeder
+     */
+    protected function resolve($class)
+    {
+        if (isset($this->container)) {
+            $instance = $this->container->make($class);
+
+            $instance->setContainer($this->container);
+        } else {
+            $instance = new $class;
+        }
+
+        if (isset($this->command)) {
+            $instance->setCommand($this->command);
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Set the IoC container instance.
+     *
+     * @param  \Illuminate\Container\Container  $container
+     * @return $this
+     */
+    public function setContainer(Container $container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+    /**
+     * Set the console command instance.
+     *
+     * @param  \Illuminate\Console\Command  $command
+     * @return $this
+     */
+    public function setCommand(Command $command)
+    {
+        $this->command = $command;
+
+        return $this;
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __invoke()
+    {
+        if (! method_exists($this, 'run')) {
+            throw new InvalidArgumentException('Method [run] missing from '.get_class($this));
+        }
+
+        return isset($this->container)
+                    ? $this->container->call([$this, 'run'])
+                    : $this->run();
+    }
+}
