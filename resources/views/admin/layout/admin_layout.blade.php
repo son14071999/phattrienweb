a0c74c4bb693698022c19afb9f342f5c1a72fd16
LongNB8 <long17000547@gmail.com>
2020-11-11 22:54:49 +0700

    admin3

--- resources/views/admin/layout/admin_layout.blade.php
+++ resources/views/admin/layout/admin_layout.blade.php
@@ -95,39 +95,7 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link nav-toggle" href="javascript:;"> <i class="fa fa-book"></i> <span class="title">Danh sách đề thi</span> <span class="arrow"></span> </a>
-            <ul class="sub-menu">
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{ URL::to('/add-exam') }}"> <span class="title">Thêm đề thi</span> </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{ URL::to('/all-exam') }}"> <span class="title">Tất cả đề thi</span> </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link nav-toggle" href="javascript:;"> <i class="fa fa-book"></i> <span class="title">Danh sách các câu hỏi</span> <span class="arrow"></span> </a>
-            <ul class="sub-menu">
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{ URL::to('/add-question') }}"> <span class="title">Thêm câu hỏi</span> </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{ URL::to('/all-question') }}"> <span class="title">Tất cả câu hỏi</span> </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link nav-toggle" href="javascript:;"> <i class="fa fa-address-card-o"></i> <span class="title">Danh sách tài khoản</span> <span class="arrow"></span> </a>
-            <ul class="sub-menu">
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{URL::to('/add-account')}}"> <span class="title">Thêm tài khoản</span> </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link nav-toggle" href="{{ URL::to('/all-account') }}"> <span class="title">Tất cả các tài khoản</span> </a>
-              </li>
-            </ul>
-          </li>
+         
           <li class="nav-item">
             <a class="nav-link nav-toggle" href="javascript:;"> <i class="fa fa-table"></i> <span class="title">Bảng xếp hạng</span> <span class="arrow"></span> </a>
             <ul class="sub-menu">
